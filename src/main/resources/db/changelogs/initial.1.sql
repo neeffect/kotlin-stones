7c2fa8b75a7efb5d63a29e48efce012069c2c512
jarek ratajski <jratajski@gmail.com>
2019-10-29 22:16:06 +0100

    sql changes

--- src/main/resources/db/changelogs/initial.1.sql
+++ src/main/resources/db/changelogs/initial.1.sql
@@ -12,3 +12,13 @@ create table STONES
 create unique index STONES_ID_UINDEX
 	on STONES (ID);
 
+create sequence LogSeq;
+
+
+create table AuditLog (
+    id bigint default LogSeq.nextval,
+    user VARCHAR not null,
+    operation VARCHAR not null,
+    operationDate TIMESTAMP not null,
+    constraint AutidLog_PK primary key (id)
+);
