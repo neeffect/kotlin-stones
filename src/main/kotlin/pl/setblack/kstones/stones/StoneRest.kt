671ac12d52b8b3a1a3ae5d94f4962270740d190b
jarek ratajski <jratajski@gmail.com>
2020-01-14 20:24:38 +0100

    async change on ktor

--- src/main/kotlin/pl/setblack/kstones/stones/StoneRest.kt
+++ src/main/kotlin/pl/setblack/kstones/stones/StoneRest.kt
@@ -1,25 +1,37 @@
 package pl.setblack.kstones.stones
 
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import pl.setblack.kstones.db.DbConnection
+import pl.setblack.nee.Nee
 import pl.setblack.nee.ctx.web.WebContext
 
 class StoneRest(private val stoneService: StoneService) {
 
     fun api(): Routing.() -> Unit = {
         get("/stones") {
-
             val stones = stoneService
                 .allStones()
-            WebContext.create(DbConnection.jdbcConfig, call).serveMessage(stones, Unit)
-//                .perform(createWebContext())(Unit)
-//                .map { it -> runBlocking  { call.respond(it) } }
+            WebContext.create(DbConnection.jdbcConfig, call).serveMessage(async { stones }, Unit)
         }
         get("/demo") {
             call.respondText("HELLO WORLD!")
         }
     }
+
+
 }
+
+fun <E, P, A> async(func: () -> Nee<WebContext, E, P, A>) =
+    (Nee.constP(WebContext.Effects.async) { _ -> Unit } as Nee<WebContext, E, P, Unit>)
+        .flatMap {
+            println("in thread " + Thread.currentThread().name)
+            func()
+        }
