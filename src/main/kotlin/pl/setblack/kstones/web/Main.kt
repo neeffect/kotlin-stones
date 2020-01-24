9ebaf90d2ddf819a2be6b6d9c8eaf9b06a1cd78b
jarek ratajski <jratajski@gmail.com>
2020-01-24 19:38:43 +0100

    async change on ktor

--- src/main/kotlin/pl/setblack/kstones/web/Main.kt
+++ src/main/kotlin/pl/setblack/kstones/web/Main.kt
@@ -1,14 +1,11 @@
 package pl.setblack.kstones.web
 
-import io.ktor.features.ContentNegotiation
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import pl.setblack.kstones.stones.StoneRepo
-import pl.setblack.kstones.stones.StoneService
-import pl.setblack.kstones.db.DbSequence
-import io.ktor.jackson.jackson
 import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import io.vavr.jackson.datatype.VavrModule
 import pl.setblack.kstones.stones.StonesModule
 
